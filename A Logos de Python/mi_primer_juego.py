ed9eebfb3c818a1e461f1e452d33e4a1f189065e
Linis_Git <linisguerrero28@gmail.com>
2026-06-20 11:31:53 -0400

    Proyecto PyGame 2026 inicial con PDF (para descargar) y recursos

---
+++ A Logos de Python/mi_primer_juego.py
@@ -0,0 +1,35 @@
+
+
+
+
+background_image_filename = 'pygame01.jpg'
+
+mouse_image_filename = 'cubo01.png'
+
+import pygame
+
+from pygame.locals import *
+
+from sys import exit
+
+pygame.init()
+
+screen = pygame.display.set_mode((640, 480), 0, 32)
+
+pygame.display.set_caption("Hola Mundo!")
+
+background = pygame.image.load(background_image_filename).convert()
+
+mouse_cursor = pygame.image.load(mouse_image_filename).convert_alpha()
+while True:
+    for event in pygame.event.get():
+        if event.type == QUIT:
+            exit()
+    screen.blit(background, (0,0))
+    x, y = pygame.mouse.get_pos()
+    x-= mouse_cursor.get_width() / 2
+    y-= mouse_cursor.get_height() / 2
+    screen.blit(mouse_cursor, (x, y))
+    pygame.display.update()
+
+
